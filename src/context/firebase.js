1168167efb33d16e449484617719071aa411e1a8
Manmeet <manmeetnagiii@gmail.com>
2024-06-27 03:43:01 +0530

    twitter frontend

--- src/context/firebase.js
+++ src/context/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC1UsxjORHhL9LfqNkl_wsTJ2rV3eDSvy0",
-    authDomain: "twitter-clone-eb901.firebaseapp.com",
-    projectId: "twitter-clone-eb901",
-    storageBucket: "twitter-clone-eb901.appspot.com",
-    messagingSenderId: "434624553161",
-    appId: "1:434624553161:web:25fea1e9cf538e6ad8ad18"
+  apiKey: "AIzaSyASdT8OslHIDRT_vtJQAvX3T8kZyvx-bvw",
+  authDomain: "twitter-clone-3287a.firebaseapp.com",
+  projectId: "twitter-clone-3287a",
+  storageBucket: "twitter-clone-3287a.appspot.com",
+  messagingSenderId: "662732993999",
+  appId: "1:662732993999:web:cb86c082fb9acd63ed4eba"
 };
 
 const app = initializeApp(firebaseConfig);
