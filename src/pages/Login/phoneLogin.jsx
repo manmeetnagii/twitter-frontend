ff700ed3e5b5318b9233fdbd93bd35bc0c28da31
Manmeet <manmeetnagiii@gmail.com>
2024-09-09 02:32:31 +0530

    d

--- src/pages/Login/phoneLogin.jsx
+++ src/pages/Login/phoneLogin.jsx
@@ -168,8 +168,8 @@ function Mobile({ userBrowser, userDevice, userOS, userIP }) {
     }
     else{
       if(userBrowser==="Chrome"){
-        toast.info("Chrome browsers require OTP authentication.");
         setEmailModal(true);
+        toast.info("Chrome browsers require OTP authentication.");
       }
       else{
         try {
