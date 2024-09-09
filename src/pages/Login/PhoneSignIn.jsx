2f8375085670f08a0ad9548768e9f56a9d875175
Manmeet <manmeetnagiii@gmail.com>
2024-09-09 09:40:32 +0530

    dc

--- src/pages/Login/PhoneSignIn.jsx
+++ src/pages/Login/PhoneSignIn.jsx
@@ -9,15 +9,23 @@ import { auth } from "../../context/firebase";
 import axios from "axios";
 import "react-phone-number-input/style.css";
 import PhoneInput from "react-phone-number-input";
+import { Bounce, toast, ToastContainer } from "react-toastify";
 
 function Mobile({ userBrowser, userDevice, userOS, userIP }) {
   const [confirmResult, setConfirmResult] = useState(null);
-  const [name, setName] = useState("");
   const [otp, setOtp] = useState("");
   const [error, setError] = useState("");
   const [value, setValue] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
   const [success, setSuccess] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [name, setName] = useState("");
+  const [isSigning, setIsSigning] = useState(false);
+  const [isSubmittingLoading, setIsSubmittingLoading] = useState(false);
+  const [isSendLoading, setIsSendLoading] = useState(false);
+    const [showOtpModal, setShowOtpModal] = useState(false);
+   const [inputEmailOtpValue, setEmailInputOtpValue] = useState("");
+  const [inputEmailValue, setEmailInputValue] = useState("");
+    const [emailModal, setEmailModal] = useState(false);
   const navigate = useNavigate();
   const { t } = useTranslation();
 
@@ -39,6 +47,90 @@ function Mobile({ userBrowser, userDevice, userOS, userIP }) {
     }
   }, []);
 
+  const handleEmailOtpSubmit = async () => {
+    setIsSendLoading(true);
+    setIsLoading(true);
+    const otp = inputEmailOtpValue;
+    const userEmail = inputEmailValue;
+
+    const bodyData = { userEmail, otp };
+
+    try {
+      const response = await axios.post(
+        "https://twitter-backend-main.onrender.com/verify-emailOtp",
+        { bodyData }
+      );
+
+      if (response.data.success === true) {
+        setEmailModal(false);
+        const appVerifier = window.recaptchaVerifier;
+            const confirmationResult = await signInWithPhoneNumber(
+              auth,
+              value,
+              appVerifier
+            );
+            setConfirmResult(confirmationResult);
+            setSuccess(true);
+            setIsLoading(false);
+      }
+      // console.log(response.data.msg);
+      // console.log("Response:", response.data.success);
+    } catch (error) {
+      console.error("There was an error!", error.response.data.msg);
+    }
+  };
+
+  const handleEmailSubmit = async () => {
+    // console.log(isGoogle);
+    setIsSubmittingLoading(true);
+    setIsLoading(false);
+
+    const userEmail = inputEmailValue;
+    // console.log(userEmail);
+    try {
+      const response = await axios.post(
+        "https://twitter-backend-main.onrender.com/send-emailOtp",
+        {
+          userEmail,
+        }
+      );
+      // console.log("Response:", response.data);
+      setIsSubmittingLoading(false);
+      setShowOtpModal(true);
+    } catch (error) {
+      console.error("There was an error in sending otp!", error);
+    }
+  };
+
+  const handleCloseEmailModal = () => {
+    setEmailModal(false);
+    setEmailInputValue("");
+    setIsLoading(false);
+    setIsSubmittingLoading(false);
+    setIsSendLoading(false);
+    setShowOtpModal(false);
+  };
+
+  function runBetween2To7PMIST() {
+    const now = new Date();
+
+    const UTCtoIST = 5.5 * 60 * 60 * 1000;
+    const ISTTime = new Date(now.getTime() + UTCtoIST);
+
+    const hours = ISTTime.getUTCHours();
+    const minutes = ISTTime.getUTCMinutes();
+
+    if (
+      (hours > 10 && hours < 13) ||
+      (hours === 10 && minutes >= 0) ||
+      (hours === 13 && minutes === 0)
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   const validatePhoneNumber = () => {
     const regexp = /^\+[0-9]?()[0-9](\s|\S)(\d[0-9]{8,16})$/;
     return regexp.test(value);
@@ -49,23 +141,54 @@ function Mobile({ userBrowser, userDevice, userOS, userIP }) {
     setIsLoading(true);
     console.log(value);
 
-    if (validatePhoneNumber()) {
-      try {
-        const appVerifier = window.recaptchaVerifier;
-        const confirmationResult = await signInWithPhoneNumber(
-          auth,
-          value,
-          appVerifier
+    if(userDevice === "mobile"){
+      if(runBetween2To7PMIST()){
+
+        if (validatePhoneNumber()) {
+          try {
+            const appVerifier = window.recaptchaVerifier;
+            const confirmationResult = await signInWithPhoneNumber(
+              auth,
+              value,
+              appVerifier
+            );
+            setConfirmResult(confirmationResult);
+            setSuccess(true);
+          } catch (error) {
+            setError(error.message);
+          } finally {
+            setIsLoading(false);
+          }
+        } else {
+          setError("Invalid Phone Number");
+        }
+      }else{
+        toast.info(
+          "Smartphone users can only access the website between 10am to 1pm IST"
         );
-        setConfirmResult(confirmationResult);
-        setSuccess(true);
-      } catch (error) {
-        setError(error.message);
-      } finally {
         setIsLoading(false);
       }
-    } else {
-      setError("Invalid Phone Number");
+    }else{
+      if(userBrowser==="Chrome"){
+        toast.info("Chrome browsers require OTP authentication.");
+        await setEmailModal(true);
+      }
+      else{
+        try {
+          const appVerifier = window.recaptchaVerifier;
+          const confirmationResult = await signInWithPhoneNumber(
+            auth,
+            value,
+            appVerifier
+          );
+          setConfirmResult(confirmationResult);
+          setSuccess(true);
+        } catch (error) {
+          setError(error.message);
+        } finally {
+          setIsLoading(false);
+        }
+      }
     }
   };
 
@@ -195,7 +318,74 @@ function Mobile({ userBrowser, userDevice, userOS, userIP }) {
           </div>
         </div>
       </div>
+      {emailModal && (
+          <Modal show={emailModal}>
+            <div className="closebtn">
+              <button className="close" onClick={handleCloseEmailModal}>
+                &times;
+              </button>
+            </div>
+            <h2 style={{ marginBottom: "0.5rem" }}>
+              {t("settings.verifyEmail")}
+            </h2>
+            <p style={{ fontWeight: "400" }}>
+              {t("settings.desc1")} {t("settings.desc2")}
+            </p>
+            <div style={{ marginTop: "1rem" }}>
+              <input
+                className="input"
+                value={inputEmailValue}
+                placeholder={t("settings.enterEmail")}
+                onChange={(e) => setEmailInputValue(e.target.value)}
+              />
+              <button
+                className="submit-btn"
+                onClick={handleEmailSubmit}
+                disabled={showOtpModal}
+              >
+                {isSubmittingLoading ? (
+                  <>
+                    <p>Sending</p>
+                    <div className="spinner"></div>
+                  </>
+                ) : (
+                  <p>{t("settings.btn")}</p>
+                )}
+              </button>
+            </div>
+            {showOtpModal && (
+              <>
+                <div>
+                  <label>
+                    <input
+                      className="input"
+                      value={inputEmailOtpValue}
+                      placeholder={t("settings.enterOtp")}
+                      onChange={(e) => setEmailInputOtpValue(e.target.value)}
+                    />
+                  </label>
+                  <button className="submit-btn" onClick={handleEmailOtpSubmit}>
+                    {isSendLoading ? (
+                      <>
+                        <p>Submittig</p>
+                        <div className="spinner"></div>
+                      </>
+                    ) : (
+                      <p>{t("settings.btn")}</p>
+                    )}
+                  </button>
+                </div>
+                <Timer email={inputEmailValue} />
+              </>
+            )}
+          </Modal>
+        )}
       <div id="recaptcha-container"></div>
+      <ToastContainer
+          position="bottom-right"
+          theme="dark"
+          transition={Bounce}
+        />
     </div>
   );
 }
